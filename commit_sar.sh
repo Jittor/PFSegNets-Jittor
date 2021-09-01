1a61f0f4b68d84482ee30077d059e58ac45e08ce
Jittor <59786298+Jittor@users.noreply.github.com>
2021-09-01 17:45:00 +0800

    Update commit_sar.sh

--- commit_sar.sh
+++ commit_sar.sh
@@ -1,4 +1,4 @@
 sudo docker build -t pfnet_sar .
 # sudo docker run --rm -it --network none  --gpus all -v gaofen/sar/val/image:/input_path -v test_img:/output_path pfnet_sar
-sudo docker tag pfnet_sar registry.cn-beijing.aliyuncs.com/glotwo/sar:pfnet_sar
-sudo docker push registry.cn-beijing.aliyuncs.com/glotwo/sar:pfnet_sar
+sudo docker tag pfnet_sar registry.cn-beijing.aliyuncs.com/xxx/sar:pfnet_sar
+sudo docker push registry.cn-beijing.aliyuncs.com/xxx/sar:pfnet_sar
